23fa3da238f31e44cec1b02391c0101aa138b8b2
Anthony Lai <yukuan.anthony.lai@gmail.com>
2015-03-11 01:59:28 -0700

    Implemented WebSteg

--- WebSteg.cpp
+++ WebSteg.cpp
@@ -1,13 +1,22 @@
 #include "provided.h"
+#include "http.h"
 #include <string>
 using namespace std;
 
 bool WebSteg::hideMessageInPage(const string& url, const string& msg, string& host)
 {
-	return false;  // This compiles, but may not be correct
+    string webPageText;
+    HTTP().get(url, webPageText);
+    if (Steg::hide(webPageText, msg, host))
+        return true;
+    return false;
 }
 
 bool WebSteg::revealMessageInPage(const string& url, string& msg)
 {
-	return false;  // This compiles, but may not be correct
+    string webPageText;
+    HTTP().get(url, webPageText);
+    if (Steg::reveal(webPageText, msg))
+        return true;
+    return false;
 }
