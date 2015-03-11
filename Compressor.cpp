0ee4aaeb596e8860d49042974864821feed8a595
Anthony Lai <yukuan.anthony.lai@gmail.com>
2015-03-10 19:42:50 -0700

    Implemented Compressor::decompress()

--- Compressor.cpp
+++ Compressor.cpp
@@ -6,18 +6,19 @@ using namespace std;
 
 void Compressor::compress(const string& s, vector<unsigned short>& numbers)
 {
-    //  Create the HashTAble
+    //  Create the HashTable
     int capacity = s.size() / 2 + 512;
     if (capacity > 16384)
         capacity = 16384;
     HashTable<string, unsigned short> wordsHash(2 * capacity, capacity);
     
-    //  Initialize all the permanent ones
+    //  Initialize the permanent ones
     int nextFreeID;
     for (nextFreeID = 0; nextFreeID < 256; nextFreeID++)
         //  using the fill string constructor to convert char to string
         wordsHash.set(string(1, static_cast<char>(nextFreeID)), nextFreeID, true);
     
+    //  Defined some variables
     string runSoFar = "";
     string expandedRun = "";
     string dummyKey = "";
@@ -60,10 +61,61 @@ void Compressor::compress(const string& s, vector<unsigned short>& numbers)
     }
     
     numbers.push_back(capacity);    //  8
-    
 }
 
 bool Compressor::decompress(const vector<unsigned short>& numbers, string& s)
 {
-	return false;  // This compiles, but may not be correct
+	//  Base condition check
+    if (numbers.size() == 0)
+        //  numbers must at least has the HashTable capacity
+        return false;
+    
+    //  Create the HashTable
+    int capacity = numbers[numbers.size() - 1];
+    HashTable<unsigned short, string> numHash(2 * capacity, capacity);
+    
+    //  Initialize the permanent ones
+    for (int i = 0; i < 256; i++)
+        //  using the fill string constructor to convert char to string
+        numHash.set(i, string(1, i), true);
+    
+    //  Defined some variables
+    int nextFreeID = 256;
+    string runSoFar = "";
+    string expandedRun = "";
+    string numValue = "";
+    string dummyValue = "";
+    s = "";
+    unsigned short discardedID;
+    
+    for (int i = 0; i < numbers.size() - 1; i++) {
+        if (numbers[i] <= 255) { //  a
+            numHash.get(numbers[i], numValue);
+            s += numValue;
+            if (runSoFar.size() == 0) { //  ii
+                runSoFar += numValue;
+                continue;
+            } else {    //  iii
+                expandedRun = runSoFar + numValue;
+                if (!numHash.isFull()) {    //  iv
+                    numHash.set(nextFreeID, expandedRun);
+                    nextFreeID++;
+                } else {    //  v
+                    numHash.discard(discardedID, dummyValue);
+                    numHash.set(discardedID, expandedRun);
+                }
+                runSoFar = "";  //  vi
+                continue;
+            }
+        } else {    //  b
+            if (!numHash.get(numbers[i], numValue))
+                //  HashTable does not have this association
+                return false;
+            
+            numHash.touch(numbers[i]);  //  ii
+            s += numValue;  //  iii
+            runSoFar = numValue;    //  iv
+        }
+    }
+    return true;
 }
