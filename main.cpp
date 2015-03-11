e87dc7f4dff6d6f7bd854e35f253d34461cd6cdc
Anthony Lai <yukuan.anthony.lai@gmail.com>
2015-03-11 02:00:12 -0700

    Test cases for WebSteg

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include "provided.h"
 #include "HashTable.h"
+#include "http.h"
 using namespace std;
 
 void HashTest();
@@ -13,6 +14,7 @@ void HashTest2();
 void BinaryTest();
 void CompressorTest();
 void StegTest();
+void WebStegTest();
 
 int main()
 {
@@ -25,12 +27,47 @@ int main()
 	// if ( ! WebSteg::revealMessageInPage("http://cs.ucla.edu", msg))
 	// 	cout << "Error revealing!" << endl;
     
-    //  HashTest();
-    //  HashTest2();
-    //  BinaryTest();
-    //  CompressorTest();
+    HashTest();
+    HashTest2();
+    BinaryTest();
+    CompressorTest();
     StegTest();
+    WebStegTest();
+    
     cerr << "Passed all tests!!" << endl;
+}
+
+void WebStegTest() {
+    string originalPageText =
+    "<html>\n"
+    "<head>\n"
+    " <title>My Pretend Web Page</title>\n"
+    "</head>\n"
+    "<body>\n"
+    "<h2>My Pretend Web Page<h1>\n"
+    "<p>\n"
+    "I wish I were creative enough to have something interesting \n"
+    "to say here.  I'm not, though, so this is a boring page.\n"
+    "</p>\n"
+    "<p>\n"
+    "Oh, well.\n"
+    "</p>\n"
+    "</body>\n"
+    "</html>\n"
+    ;
+    HTTP().set("http://boring.com", originalPageText);
+    string plan =
+    "Lefty and Mugsy enter the bank by the back door.\n"
+    "Shorty waits out front in the getaway car.\n"
+    "Don't let the teller trip the silent alarm.\n"
+    ;
+    string newPageText;
+    assert(WebSteg::hideMessageInPage("http://boring.com", plan, newPageText));
+    HTTP().set("http://boring.com", newPageText);
+    string msg;
+    assert(WebSteg::revealMessageInPage("http://boring.com", msg));
+    assert(msg == plan);
+    cerr << "Recovered text is what was hidden:\n" << msg;
 }
 
 void StegTest() {
